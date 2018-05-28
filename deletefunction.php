51959972971bebb89ae846e269b18b740baaa35d
cg090 <38158103+cg090@users.noreply.github.com>
2018-05-28 12:09:48 +0200

    Add files via upload

---
+++ deletefunction.php
@@ -0,0 +1,27 @@
+<?php
+{
+    $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de;dbname=u-cg090', 'cg090', 'ahchi8Uyie',array('charset'=>'utf8'));
+
+    $statement = $pdo->prepare("SELECT * FROM posts") ;
+    if($statement->execute()) {
+        while($row=$statement->fetch()) {
+            echo $row['content'];
+            echo "<br>";
+        }
+    } else    {
+        echo "Datenbank-Fehler:";
+        echo $statement->errorInfo()[2];
+        echo $statement->queryString;
+        die();
+    }
+}
+$sql=("DELETE FROM `posts` WHERE id='".($_GET['id'])."'");;
+
+
+if ($pdo->query($sql) === TRUE) {
+    echo "Datei konnte nicht gelöscht werden.";
+} else {
+    echo "Datei wurde gelöscht.";
+}
+?>
+
