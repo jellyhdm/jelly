3e6ba23c5959c6c5655cc17b0ce4a7a8e8c7c59c
cg090 <38158103+cg090@users.noreply.github.com>
2018-07-10 20:16:08 +0200

    Update language_start.js

--- public/language_start.js
+++ public/language_start.js
@@ -1,27 +1,24 @@
-{
-    "config": {
-    "default": "de"
-}
-    "language":
-            "de":{
+var arrLang = {
 
-            "#nav-jelly":"Jelly"
-            "#nav-user":"Nutzer"
-            "#nav-search":"Suchen"
-            "#nav-over":"Übersicht"
-            "#nav-upload":"Hochladen"
-            "#nav-download":"Runterladen"
-            "#overview":"Übersicht"
-        }
+    "de": {
+        "#nav-jelly": "Jelly",
+        "#nav-user": "Nutzer",
+        "#nav-search": "Suchen",
+        "#nav-over": "Übersicht",
+        "#nav-upload": "Hochladen",
+        "#nav-download": "Runterladen",
+        "#overview": "Übersicht"
+    },
+
+    "en": {
+
+        "#nav-jelly": "Jelly",
+        "#nav-user": "User",
+        "#nav-search": "search",
+        "#nav-over": "Overview",
+        "#nav-upload": "Upload",
+        "#nav-download": "Download",
+        "#overview": "Overview"
+
     }
-            "en": {
-
-             "#nav-jelly":"Jelly"
-             "#nav-user":"User"
-             "#nav-search":"search"
-             "#nav-over":"Overview"
-             "#nav-upload":"Upload"
-             "#nav-download":"Download"
-             "#overview":"Overview"
-
-        }
+}
