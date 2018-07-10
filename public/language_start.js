c4ea23d9ae9d7bafa3a2eea0508bec5d048e66d7
cg090 <38158103+cg090@users.noreply.github.com>
2018-07-10 20:03:04 +0200

    Sprache für die Übersichtsseite

---
+++ public/language_start.js
@@ -0,0 +1,27 @@
+{
+    "config": {
+    "default": "de"
+}
+    "language":
+            "de":{
+
+            "#nav-jelly":"Jelly"
+            "#nav-user":"Nutzer"
+            "#nav-search":"Suchen"
+            "#nav-over":"Übersicht"
+            "#nav-upload":"Hochladen"
+            "#nav-download":"Runterladen"
+            "#overview":"Übersicht"
+        }
+    }
+            "en": {
+
+             "#nav-jelly":"Jelly"
+             "#nav-user":"User"
+             "#nav-search":"search"
+             "#nav-over":"Overview"
+             "#nav-upload":"Upload"
+             "#nav-download":"Download"
+             "#overview":"Overview"
+
+        }
