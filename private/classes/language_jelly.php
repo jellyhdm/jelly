06a25d059bdad83f0554ead0058ccbdab055ea2b
cg090 <38158103+cg090@users.noreply.github.com>
2018-07-10 19:16:31 +0200

    Add files via upload

---
+++ private/classes/language_jelly.php
@@ -0,0 +1,14 @@
+<?php
+
+class language {
+    public $data;
+
+    function __construct($language) {
+        $data = file_get_contents($language . ".json");
+        $this->data = json_decode($data);
+    }
+    function translate() {
+        return $this->data;
+    }
+}
+?>
