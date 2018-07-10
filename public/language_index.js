7050b05bbbf4878b6c828ff323463f3d3d54286d
cg090 <38158103+cg090@users.noreply.github.com>
2018-07-10 20:02:28 +0200

    Spache für die Index Datei

---
+++ public/language_index.js
@@ -0,0 +1,49 @@
+{
+    "config": {
+    "default": "de"
+}
+    "language":
+        "de": {
+            "#nav-jelly":"Jelly"
+            "#nav-about":"Über Jelly"
+            "#nav-features":"Features"
+            "#nav-registration":"Registrieren"
+            "#nav-login":"Login"
+        }
+
+            {
+            "#jelly": "Das etwas andere File-Sharing für deine Dateien."
+
+            "#is-jelly":"Was ist Jelly? Jelly ist ein kostenloses Filehosting-System für deine Dateien. Du kannst Jelly immer und überall nutzen, egal von welchem Gerät. Nutze Jelly um deine Dateien schnell und einfach hochzuladen. Registiere dich einfach mit deiner E-Mailadresse bei Jelly und schon kann du loslegen und deine Dateien bequem speichern. Die Bedienung von Jelly ist ganz einfach also registriere dich schnell."
+            "#can-jelly":"Was kann Jelly? Du kannst deine Dateien ganz einfach hochladen und online speichern. Lege dir verschiedene Ordner an und verschiebe deine Dateien leicht in deinem eigenen System. Du kannst die Dateien mit anderen Nutzern oder auch Nicht-Nutzern teilen und das Beste ist, du kannst auf deine Dateien jederzeit und von allen Geräten zugreifen."
+
+
+            "#registration": "Registriere dich jetzt und nutze Jelly kostenlos!"
+            "#regi-mail": "Deine E-Mail"
+            "#regi-password": "Dein Passwort"
+            "#regi-password2": "Passwort"
+        }
+            }
+
+
+        "en": {
+            "#nav-jelly":"Jelly"
+            "#nav-about":"About Jelly"
+            "#nav-features":"Features"
+            "#nav-registration":"Register"
+            "#nav-login":"Login"
+
+            "#jelly": "The different file sharing for your files."
+
+            "#is-jelly":"What is Jelly? Jelly is a free file hosting system for your files. You can use Jelly anytime and anywhere, no matter what device you have. Use Jelly to upload your files quickly and easily. Simply register with your email address at Jelly and you are ready to save your files conveniently. The operation of Jelly is very easy so register quickly."
+            "#can-jelly":"Was kann Jelly? You can easily upload your files and save them online. Create different folders and easily move your files in your own system. You can share the files with other users or even non-users. And best of all, you can access your files anytime and from any device."
+
+
+            "#registration": "Register now and use Jelly for free!"
+            "#regi-mail": "Your e-Mail"
+            "#regi-password": "Your Password"
+            "#regi-password2": "Password"
+
+        }
+
+
