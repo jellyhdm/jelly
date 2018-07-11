64cc458934ee565cd1f701ba3a6ec3c34a7f2b79
Pooh97 <mb291@hdm-stuttgart.de>
2018-07-11 12:11:55 +0200

    Mit Ausrufezeichen

--- upload/src/foundationphp/UploadFile.php
+++ upload/src/foundationphp/UploadFile.php
@@ -74,9 +74,9 @@ class UploadFile
 
 	public function allowAllTypes($suffix = null)
 	{
-		$this->typeCheckingOn = false;
+	 $this->typeCheckingOn = false;
 		if (!is_null($suffix)) {
-			if (strpos($suffix, '.') === 0 || $suffix == '') {
+		if (strpos($suffix, '.') === 0 || $suffix == '') {
 				$this->suffix = $suffix;
 			} else {
 				$this->suffix = ".$suffix";
@@ -210,7 +210,7 @@ class UploadFile
         //? : ist if, dann else - Abfrage, weil der newName nur benutzt wird, wenn der File renamed wird
         $filename = isset($this->newName) ? $this->newName : $file['name'];
         $owner_id= $_SESSION["user_session"];
-        if (is_dir($this->destination.$owner_id. '/')) {
+        if (!is_dir($this->destination.$owner_id. '/')) {
             mkdir($this->destination. $owner_id. '/', 0777);
         }
         $success = move_uploaded_file($file['tmp_name'], $this->destination .$owner_id .'/'.$filename);
