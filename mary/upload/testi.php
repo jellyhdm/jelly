ebddae078131df8589b6a20571f369ce694ae415
Pooh97 <mb291@hdm-stuttgart.de>
2018-07-13 17:18:36 +0200

    sharing versuch

--- mary/upload/testi.php
+++ mary/upload/testi.php
@@ -1,4 +1,9 @@
-<?php ?>
+<?php
+include_once '../private/includes/dbconfig.php';
+include_once '../private/includes/session.php';?>
+
 <form action="share.php" method="post" >
-    <input type="text" name="file_id" value="3">
+    File ID<input type="text" name="file_id" value="3"><br>
+    User id to has access to this share:<input type="text" name="user_share_id" value="3"><br>
+    Public share? :<input type="text" name="public_share" value="1"><br>
     <input type="submit" value="test"> </form>
